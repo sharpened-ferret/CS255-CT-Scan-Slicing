3d83b47b9e85e92511402c9d98ccae628c8bfebe
Neil Woodhouse <clonesupreme@gmail.com>
2021-01-25 23:49:32 +0000

    Add project files.

---
+++ Test.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace CWIdeaTest
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Test));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sliceTop = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(117, 261);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 0;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(279, 261);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 1;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(451, 261);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(984, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // sliceTop
+            // 
+            this.sliceTop.Location = new System.Drawing.Point(128, 312);
+            this.sliceTop.Name = "sliceTop";
+            this.sliceTop.Size = new System.Drawing.Size(75, 23);
+            this.sliceTop.TabIndex = 4;
+            this.sliceTop.Text = "Slice";
+            this.sliceTop.UseVisualStyleBackColor = true;
+            this.sliceTop.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(55, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 161);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(253, 72);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(182, 161);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(451, 72);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(182, 161);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Test
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.sliceTop);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Test";
+            this.Text = "Test";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.Button sliceTop;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Test.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CWIdeaTest
+{
+    public partial class Test : Form
+    {
+        short[,,] cthead;
+        short min, max; //min/max value in the 3D volume data set
+        int CT_x_axis = 256;
+        int CT_y_axis = 256;
+        int CT_z_axis = 113;
+
+        Bitmap topImage;
+
+
+        public Test()
+        {
+            InitializeComponent();
+            ReadData();
+
+            int Top_width = CT_x_axis;
+            int Top_height = CT_y_axis;
+
+            int Front_width = CT_x_axis;
+            int Front_height = CT_z_axis;
+
+            topImage = new Bitmap(Top_width, Top_height);
+            //pictureBox1.Image = topImage;
+
+        }
+
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            topImage = TopDownSlice(topImage, 32);
+            //PictureBox topBox = new PictureBox();
+            pictureBox1.Image = topImage;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            //topBox.Visible = true;
+
+            Console.WriteLine("Test");
+            Console.WriteLine(topImage.GetPixel(100,100).ToString());
+        }
+
+        public void ReadData()
+        {
+            // FileReader headFile = System.IO.File.Open("CThead", System.IO.FileMode.Open);
+
+            BinaryReader reader = new BinaryReader(File.Open("CThead", FileMode.Open));
+
+            int i, j, k;
+            //int i = 0;
+            //int k = 0;
+            //int j = 0;
+
+
+
+            min = short.MaxValue;
+            max = short.MinValue;
+            short read;
+            int b1, b2;
+
+            cthead = new short[CT_z_axis, CT_y_axis, CT_x_axis];
+            Int32 count = 0;
+            Int32 outerCount = 0;
+
+            string fileName = @"C:\Temp\MaheshTXFI.txt";
+            FileInfo fi = new FileInfo("CThead");
+
+            if (fi.Exists)
+            {
+                // Get file size  
+                long size = fi.Length;
+                Console.WriteLine("File Size in Bytes: {0}", size);
+                // File ReadOnly ?  
+                bool IsReadOnly = fi.IsReadOnly;
+                Console.WriteLine("Is ReadOnly: {0}", IsReadOnly);
+                // Creation, last access, and last write time   
+                DateTime creationTime = fi.CreationTime;
+                Console.WriteLine("Creation time: {0}", creationTime);
+                DateTime accessTime = fi.LastAccessTime;
+                Console.WriteLine("Last access time: {0}", accessTime);
+                DateTime updatedTime = fi.LastWriteTime;
+                Console.WriteLine("Last write time: {0}", updatedTime);
+            }
+
+            for (k=0; k < CT_z_axis; k++)
+            {
+                for (j=0; j < CT_y_axis; j++)
+                {
+                    for (i=0; i < CT_x_axis; i++)
+                    {
+                        b1 = ((int)reader.ReadByte()) & 0xff;
+                        b2 = ((int)reader.ReadByte()) & 0xff;
+                        read = (short)((b2 << 8) | b1);
+                        //read = (short)reader.ReadByte();
+                        if (read < min) min = read;
+                        if (read > max) max = read;
+                        cthead[k, j, i] = read;
+                    }
+                }
+            }
+            Console.WriteLine(min + " " + max);
+        }
+
+        public Bitmap TopDownSlice(Bitmap topImage, short sliceNumber)
+        {
+            int w = topImage.Width;
+            int h = topImage.Height;
+
+            int col;
+            double datum;
+            Bitmap returnScanSlice = new Bitmap(w, h);
+
+            for (int j = 0; j < h; j++)
+            {
+                for (int i = 0; i < w; i++)
+                {
+                    //at this point (i,j) is a single pixel in the image
+                    //here you would need to do something to (i,j) if the image size
+                    //does not match the slice size (e.g. during an image resizing operation
+                    //If you don't do this, your j,i could be outside the array bounds
+                    //In the framework, the image is 256x256 and the data set slices are 256x256
+                    //so I don't do anything - this also leaves you something to do for the assignment
+                    datum = cthead[sliceNumber, j, i]; //get values from slice 76 (change this in your assignment)
+                                                       //calculate the colour by performing a mapping from [min,max] -> 0 to 1 (float)
+                                                       //Java setColor uses float values from 0 to 1 rather than 0-255 bytes for colour
+                    //Console.WriteLine(datum);
+                    col = (int)(((float)(datum - min) / ((float)(max - min)))*255);
+                    //Console.WriteLine(col);
+                    returnScanSlice.SetPixel(i, j, Color.FromArgb(255, col, col, col));
+                }
+            }
+
+            return returnScanSlice;
+        }
+    }
+}
